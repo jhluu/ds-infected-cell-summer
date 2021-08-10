7f047dd1481650b058914a21f5615f1891e210bc
jhluu <luu.jacklyn@gmail.com>
2021-08-09 18:46:50 -0700

    update about page

--- infected-cell-portal/src/components/infected-cell-pages/About.jsx
+++ infected-cell-portal/src/components/infected-cell-pages/About.jsx
@@ -8,36 +8,44 @@ import cs from "./About.module.scss";
 import Heading from "./Heading.jsx";
 import ConsortiaMemberBoxes from "./ConsortiaMembers.jsx";
 
-class AboutLemur extends Component {
+class AboutIC extends Component {
   render() {
     return (
       <div className={cs.content}>
         <Heading title="About" />
         <Container maxWidth="lg">
           <Box my={4}>
-            <div className={cs.center}>
-              <img src={"../../../images/open_lemur.png"} width="300" />
-            </div>
+            
             <div className={cs.body}>
-              Add description of the project (
-              <a
-                href="https://www.cell.com/cell-reports/fulltext/S2211-1247(21)00173-X?_returnURL=https%3A%2F%2Flinkinghub.elsevier.com%2Fretrieve%2Fpii%2FS221112472100173X%3Fshowall%3Dtrue"
-                target="_blank"
-              >
-                Kulsuptrakul et al (2021) Cell Reports
-              </a>
-              ). And more
+              The Infected Cell Project presents a unique oppotunity to understand
+              <li> <b>viruses as a threat:</b> defining essential host pathways for infection 
+              and building assays for anti-viral screening</li>
+              <li> <b>viruses as a tool:</b> following viruses to illuminate human cell biology </li>
+              <li> <b>viruses as a forcing function:</b> to integrate Biohub’s multi-disciplinary expertise</li>
+              
               <p />
-              and more <a href="/whereisthedata">Data Release Policy</a> before
-              you use it and to reach out using the{" "}
+              The challenge we aim to solve is how can we build approaches that can address both the diversity 
+              of viral families and the diversity of cellular responses that they generate. 
+              
+              <p />
+              This current version of the Infected Cell Project utilizes CRISPR screen data from 10+ families 
+              of viruses in order to explore host-factor interactions. This enhances our understanding 
+              of the mechanisms of viral infection and helps to develop new technologies that 
+              will lead to actionable diagnostics and effective therapies. 
+              <div className={cs.center}>
+              <img src={"../../../images/opencell_logo.png"} width="300" />
+              </div>
+
+              <p />
+              Our goal is to make sequence data rapidly and broadly available to the scientific community 
+              as a community resource. Before you use our data, please read our <a href="/whereisthedata">Data Release Policy </a>
+              and feel free to reach out to our group using the{" "}
               <a
                 href="https://docs.google.com/forms/d/e/1FAIpQLSeWMqdbrCtSZ2U-cp7OELswEMVchPUJZ-L8REwQf3-nC0JmWQ/viewform?usp=sf_link"
                 target="_blank"
               >
-                contact form
+                contact form.
               </a>
-              . Redistribution of these data should include the full text of the
-              data use policy.
             </div>
           </Box>
         </Container>
@@ -59,4 +67,4 @@ class AboutLemur extends Component {
   }
 }
 
-export default AboutLemur;
+export default AboutIC;
